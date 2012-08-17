5a645632e1ea93bf0dd8876a1b6f0c8e35eef5bb
Jack Dempsey <jack.dempsey@gmail.com>
2012-08-16 21:23:19 -0700

    added rspec

--- lib/attachments/engine.rb
+++ lib/attachments/engine.rb
@@ -1,5 +1,10 @@
 module Attachments
   class Engine < ::Rails::Engine
+    config.generators do |g|
+      g.test_framework :rspec
+      g.integration_tool :rspec
+    end
+
     isolate_namespace Attachments
   end
 end
